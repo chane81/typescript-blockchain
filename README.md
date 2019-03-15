a7c7d070e16e59b43c44360017fb6e53c75cb522
chane81 <chane81@naver.com>
2019-03-16 02:17:14 +0900

    typescript 세팅

---
+++ README.md
@@ -0,0 +1,16 @@
+# typescript
+
+## yarn
+  > yarn global add typescript
+
+## 타입스크립트 config 파일 생성
+  > tsc --init
+
+## package.json 설정
+  ```json
+  "scripts": {
+		"start": "node index.js",
+		"prestart": "tsc"
+	}
+  ```
+
---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = 'Lee', age = 30, gender = 'male';
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", you are a " + gender);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
