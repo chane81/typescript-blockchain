92a85e048d3a73b5e6bb8ca90f2b50a30afa3bde
chane81 <chane81@naver.com>
2019-03-16 03:30:16 +0900

    인터페이스 사용

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var sayHi = function (name, age, gender) {
-    return "Hello " + name + ", you are " + age + ", you are a " + gender;
-};
-console.log(sayHi("BM", 10, "male"));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-	return `Hello ${name}, you are ${age}, you are a ${gender}`;
+
+// 함수 파라메터에 type 적용
+// const sayHi = (name: string, age: number, gender: string): string => {
+// 	return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// };
+
+// console.log(sayHi("BM", 10, "male"));
+
+// export {};
+
+
+
+
+// 인터페이스 사용
+interface Human {
+	name: string;
+	age: number;
+	gender: string;
+}
+
+const person = {
+	name: 'lee',
+	age: 7,
+	gender: 'male'
+}
+
+const sayHi = (presonInfo: Human): string => {
+	return `Hello ${presonInfo.name}, you are ${presonInfo.age}, you are a ${presonInfo.gender}`;
 };
 
-console.log(sayHi("BM", 10, "male"));
+console.log(sayHi(person));
 
 export {};
